b078beb138d3d971953948cf5f61c33b5694789f
Chris Partridge <chris@partridge.tech>
2020-11-03 14:25:13 -0500

    Loads current version of twint

--- README.md
+++ README.md
@@ -1,70 +1,19 @@
-# TWINT - Twitter Intelligence Tool
-![2](https://i.imgur.com/iaH3s7z.png)
-![3](https://i.imgur.com/hVeCrqL.png)
+# twint
 
-[![PyPI](https://img.shields.io/pypi/v/twint.svg)](https://pypi.org/project/twint/) [![Build Status](https://travis-ci.org/twintproject/twint.svg?branch=master)](https://travis-ci.org/twintproject/twint) [![Python 3.6|3.7|3.8](https://img.shields.io/badge/Python-3.6%2F3.7%2F3.8-blue.svg)](https://www.python.org/download/releases/3.0/) [![GitHub license](https://img.shields.io/github/license/haccer/tweep.svg)](https://github.com/haccer/tweep/blob/master/LICENSE) [![Downloads](https://pepy.tech/badge/twint)](https://pepy.tech/project/twint) [![Downloads](https://pepy.tech/badge/twint/week)](https://pepy.tech/project/twint/week) [![Patreon](https://img.shields.io/endpoint.svg?url=https:%2F%2Fshieldsio-patreon.herokuapp.com%2Ftwintproject)](https://www.patreon.com/twintproject) ![](https://img.shields.io/twitter/follow/noneprivacy.svg?label=Follow&style=social) 
+This is a fork of [twintproject/twint](https://github.com/twintproject/twint) which is focused on Elasticsearch compatibility, performance, and DeepCISO's specific needs. When in doubt, we tend to rip things out (ex. geolocation in tweets, geopy dependency), so this is not 100% compatible with twint upstream.
 
->No authentication. No API. No limits.
-
-Twint is an advanced Twitter scraping tool written in Python that allows for scraping Tweets from Twitter profiles **without** using Twitter's API.
-
-Twint utilizes Twitter's search operators to let you scrape Tweets from specific users, scrape Tweets relating to certain topics, hashtags & trends, or sort out *sensitive* information from Tweets like e-mail and phone numbers. I find this very useful, and you can get really creative with it too.
-
-Twint also makes special queries to Twitter allowing you to also scrape a Twitter user's followers, Tweets a user has liked, and who they follow **without** any authentication, API, Selenium, or browser emulation.
-
-## tl;dr Benefits
-Some of the benefits of using Twint vs Twitter API:
-- Can fetch almost __all__ Tweets (Twitter API limits to last 3200 Tweets only);
-- Fast initial setup;
-- Can be used anonymously and without Twitter sign up;
-- **No rate limitations**.
-
-## Limits imposed by Twitter
-Twitter limits scrolls while browsing the user timeline. This means that with `.Profile` or with `.Favorites` you will be able to get ~3200 tweets.
-
-## Requirements
-- Python 3.6;
-- aiohttp;
-- aiodns;
-- beautifulsoup4;
-- cchardet;
-- dataclasses
-- elasticsearch;
-- pysocks;
-- pandas (>=0.23.0);
-- aiohttp_socks;
-- schedule;
-- geopy;
-- fake-useragent;
-- py-googletransx.
+We will not be supporting this in its current form. Issues requesting support will be closed without response.
 
 ## Installing
 
-**Git:**
 ```bash
-git clone --depth=1 https://github.com/twintproject/twint.git
-cd twint
+git clone --depth=1 https://github.com/DeepCISO/elastwint.git
+cd elastwint
 pip3 install . -r requirements.txt
 ```
 
-**Pip:**
-```bash
-pip3 install twint
-```
-
-or
-
-```bash
-pip3 install --user --upgrade git+https://github.com/twintproject/twint.git@origin/master#egg=twint
-```
-
-**Pipenv**:
-```bash
-pipenv install git+https://github.com/twintproject/twint.git#egg=twint
-```
-
-## CLI Basic Examples and Combos
-A few simple examples to help you understand the basics:
+## CLI
+A few simple examples:
 
 - `twint -u username` - Scrape all the Tweets of a *user* (doesn't include **retweets** but includes **replies**).
 - `twint -u username -s pineapple` - Scrape all Tweets from the *user*'s timeline containing _pineapple_.
@@ -88,124 +37,4 @@ A few simple examples to help you understand the basics:
 - `twint -u username --retweets` - Use a quick method to gather the last 900 Tweets (that includes retweets) from a user's profile.
 - `twint -u username --resume resume_file.txt` - Resume a search starting from the last saved scroll-id.
 
-More detail about the commands and options are located in the [wiki](https://github.com/twintproject/twint/wiki/Commands)
-
-## Module Example
-
-Twint can now be used as a module and supports custom formatting. **More details are located in the [wiki](https://github.com/twintproject/twint/wiki/Module)**
-
-```python
-import twint
-
-# Configure
-c = twint.Config()
-c.Username = "realDonaldTrump"
-c.Search = "great"
-
-# Run
-twint.run.Search(c)
-```
-> Output
-
-`955511208597184512 2018-01-22 18:43:19 GMT <now> pineapples are the best fruit`
-
-```python
-import twint
-
-c = twint.Config()
-
-c.Username = "noneprivacy"
-c.Custom["tweet"] = ["id"]
-c.Custom["user"] = ["bio"]
-c.Limit = 10
-c.Store_csv = True
-c.Output = "none"
-
-twint.run.Search(c)
-```
-
-## Storing Options
-- Write to file;
-- CSV;
-- JSON;
-- SQLite;
-- Elasticsearch.
-
-## Elasticsearch Setup
-
-Details on setting up Elasticsearch with Twint is located in the [wiki](https://github.com/twintproject/twint/wiki/Elasticsearch).
-
-## Graph Visualization
-![graph](https://i.imgur.com/EEJqB8n.png)
-
-[Graph](https://github.com/twintproject/twint/wiki/Graph) details are also located in the [wiki](https://github.com/twintproject/twint/wiki/Graph).
-
-We are developing a Twint Desktop App.
-
-![4](https://i.imgur.com/DzcfIgL.png)
-
-## FAQ
-> I tried scraping tweets from a user, I know that they exist but I'm not getting them
-
-Twitter can shadow-ban accounts, which means that their tweets will not be available via search. To solve this, pass `--profile-full` if you are using Twint via CLI or, if are using Twint as module, add `config.Profile_full = True`. Please note that this process will be quite slow.
-## More Examples
-
-#### Followers/Following
-
-> To get only follower usernames/following usernames
-
-`twint -u username --followers`
-
-`twint -u username --following`
-
-> To get user info of followers/following users
-
-`twint -u username --followers --user-full`
-
-`twint -u username --following --user-full`
-
-#### userlist
-
-> To get only user info of user
-
-`twint -u username --user-full`
-
-> To get user info of users from a userlist
-
-`twint --userlist inputlist --user-full`
-
-
-#### tweet translation (experimental)
-
-> To get 100 english tweets and translate them to italian
-
-`twint -u noneprivacy --csv --output none.csv --lang en --translate --translate-dest it --limit 100`
-
-or
-
-```python
-import twint
-
-c = twint.Config()
-c.Username = "noneprivacy"
-c.Limit = 100
-c.Store_csv = True
-c.Output = "none.csv"
-c.Lang = "en"
-c.Translate = True
-c.TranslateDest = "it"
-twint.run.Search(c)
-```
-
-Notes:
-- [Google translate has some quotas](https://cloud.google.com/translate/quotas)
-
-## Featured Blog Posts:
-- [How to use Twint as an OSINT tool](https://pielco11.ovh/posts/twint-osint/)
-- [Basic tutorial made by Null Byte](https://null-byte.wonderhowto.com/how-to/mine-twitter-for-targeted-information-with-twint-0193853/)
-- [Analyzing Tweets with NLP in minutes with Spark, Optimus and Twint](https://towardsdatascience.com/analyzing-tweets-with-nlp-in-minutes-with-spark-optimus-and-twint-a0c96084995f)
-- [Loading tweets into Kafka and Neo4j](https://markhneedham.com/blog/2019/05/29/loading-tweets-twint-kafka-neo4j/)
-
-## Contact
-
-If you have any question, want to join in discussions, or need extra help, you are welcome to join our Twint focused channel at [OSINT team](https://osint.team)
+More detail about the commands and options are located in the [twint wiki](https://github.com/twintproject/twint/wiki/Commands)
--- twint/storage/elasticsearch.py
+++ twint/storage/elasticsearch.py
@@ -1,8 +1,9 @@
 ## TODO - Fix Weekday situation
 from elasticsearch import Elasticsearch, helpers
-from geopy.geocoders import Nominatim
 from datetime import datetime
 import contextlib
+import time
+import traceback
 import sys
 
 _index_tweet_status = False
@@ -12,27 +13,11 @@ _is_near_def = False
 _is_location_def = False
 _near = {}
 _location = {}
-
-geolocator = Nominatim(user_agent="twint-1.2")
+_es = False
 
 class RecycleObject(object):
     def write(self, junk): pass
     def flush(self): pass
-
-def getLocation(place, **options):
-    location = geolocator.geocode(place,timeout=1000)
-    if location:
-        if options.get("near"):
-            global _near
-            _near = {"lat": location.latitude, "lon": location.longitude}
-            return True
-        elif options.get("location"):
-            global _location
-            _location = {"lat": location.latitude, "lon": location.longitude}
-            return True
-        return {"lat": location.latitude, "lon": location.longitude}
-    else:
-        return {}
 
 def handleIndexResponse(response):
     try:
@@ -54,131 +39,125 @@ def handleIndexResponse(response):
 def createIndex(config, instance, **scope):
     if scope.get("scope") == "tweet":
         tweets_body = {
-                "mappings": {
-                    "properties": {
-                        "id": {"type": "long"},
-                        "conversation_id": {"type": "long"},
-                        "created_at": {"type": "text"},
-                        "date": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
-                        "timezone": {"type": "keyword"},
-                        "place": {"type": "keyword"},
-                        "location": {"type": "keyword"},
-                        "tweet": {"type": "text"},
-                        "lang": {"type": "keyword"},
-                        "hashtags": {"type": "keyword", "normalizer": "hashtag_normalizer"},
-                        "cashtags": {"type": "keyword", "normalizer": "hashtag_normalizer"},
-                        "user_id_str": {"type": "keyword"},
-                        "username": {"type": "keyword", "normalizer": "hashtag_normalizer"},
-                        "name": {"type": "text"},
-                        "profile_image_url": {"type": "text"},
-                        "day": {"type": "integer"},
-                        "hour": {"type": "integer"},
-                        "link": {"type": "text"},
-                        "retweet": {"type": "text"},
-                        "essid": {"type": "keyword"},
-                        "nlikes": {"type": "integer"},
-                        "nreplies": {"type": "integer"},
-                        "nretweets": {"type": "integer"},
-                        "quote_url": {"type": "text"},
-                        "video": {"type":"integer"},
-                        "thumbnail": {"type":"text"},
-                        "search": {"type": "text"},
-                        "near": {"type": "text"},
-                        "geo_near": {"type": "geo_point"},
-                        "geo_tweet": {"type": "geo_point"},
-                        "photos": {"type": "text"},
-                        "user_rt_id": {"type": "keyword"},
-                        "mentions": {"type": "keyword", "normalizer": "hashtag_normalizer"},
-                        "source": {"type": "keyword"},
-                        "user_rt": {"type": "keyword"},
-                        "retweet_id": {"type": "keyword"},
-                        "reply_to": {
-                            "type": "nested",
-                            "properties": {
-                                "user_id": {"type": "keyword"},
-                                "username": {"type": "keyword"}
-                            }
-                        },
-                        "retweet_date": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss", "ignore_malformed": True},
-                        "urls": {"type": "keyword"},
-                        "translate": {"type": "text"},
-                        "trans_src": {"type": "keyword"},
-                        "trans_dest": {"type": "keyword"},
+            "mappings": {
+                "properties": {
+                    "id": {"type": "long"},
+                    "conversation_id": {"type": "long"},
+                    "created_at": {"type": "text"},
+                    "date": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
+                    "timezone": {"type": "keyword"},
+                    "location": {"type": "keyword"},
+                    "tweet": {"type": "text"},
+                    "lang": {"type": "keyword"},
+                    "hashtags": {"type": "keyword", "normalizer": "hashtag_normalizer"},
+                    "cashtags": {"type": "keyword", "normalizer": "hashtag_normalizer"},
+                    "user_id_str": {"type": "keyword"},
+                    "username": {"type": "keyword", "normalizer": "hashtag_normalizer"},
+                    "name": {"type": "text"},
+                    "profile_image_url": {"type": "text"},
+                    "day": {"type": "integer"},
+                    "hour": {"type": "integer"},
+                    "link": {"type": "text"},
+                    "retweet": {"type": "text"},
+                    "essid": {"type": "keyword"},
+                    "nlikes": {"type": "integer"},
+                    "nreplies": {"type": "integer"},
+                    "nretweets": {"type": "integer"},
+                    "quote_url": {"type": "text"},
+                    "video": {"type":"integer"},
+                    "thumbnail": {"type":"text"},
+                    "search": {"type": "text"},
+                    "near": {"type": "text"},
+                    "photos": {"type": "text"},
+                    "user_rt_id": {"type": "keyword"},
+                    "mentions": {
+                        "type": "nested",
+                        "properties": {
+                            "screen_name": {"type": "keyword"},
+                            "name": {"type": "keyword"},
+                            "id": {"type": "keyword"}
                         }
                     },
-                    "settings": {
-                        "number_of_shards": 1,
-                        "analysis": {
-                            "normalizer": {
-                                "hashtag_normalizer": {
-                                    "type": "custom",
-                                    "char_filter": [],
-                                    "filter": ["lowercase", "asciifolding"]
-                                }
-                            }
+                    "source": {"type": "keyword"},
+                    "user_rt": {"type": "keyword"},
+                    "retweet_id": {"type": "keyword"},
+                    "reply_to": {
+                        "type": "nested",
+                        "properties": {
+                            "screen_name": {"type": "keyword"},
+                            "name": {"type": "keyword"},
+                            "id": {"type": "keyword"}
+                        }
+                    },
+                    "retweet_date": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss", "ignore_malformed": True},
+                    "urls": {"type": "keyword"},
+                    "translate": {"type": "text"},
+                    "trans_src": {"type": "keyword"},
+                    "trans_dest": {"type": "keyword"},
+                }
+            },
+            "settings": {
+                "number_of_shards": 2,
+                "analysis": {
+                    "normalizer": {
+                        "hashtag_normalizer": {
+                            "type": "custom",
+                            "char_filter": [],
+                            "filter": ["lowercase", "asciifolding"]
                         }
                     }
                 }
-        with nostdout():
-            resp = instance.indices.create(index=config.Index_tweets, body=tweets_body, ignore=400)
+            }
+        }
+        resp = instance.indices.create(index=config.Index_tweets, body=tweets_body, ignore=400)
         return handleIndexResponse(resp)
     elif scope.get("scope") == "follow":
         follow_body = {
-                "mappings": {
-                    "properties": {
-                        "user": {"type": "keyword"},
-                        "follow": {"type": "keyword"},
-                        "essid": {"type": "keyword"}
-                        }
-                    },
-                    "settings": {
-                        "number_of_shards": 1
-                    }
+            "mappings": {
+                "properties": {
+                    "user": {"type": "keyword"},
+                    "follow": {"type": "keyword"},
+                    "essid": {"type": "keyword"}
                 }
-        with nostdout():
-            resp = instance.indices.create(index=config.Index_follow, body=follow_body, ignore=400)
+            },
+            "settings": {
+                "number_of_shards": 2
+            }
+        }
+        resp = instance.indices.create(index=config.Index_follow, body=follow_body, ignore=400)
         return handleIndexResponse(resp)
     elif scope.get("scope") == "user":
         user_body = {
-                "mappings": {
-                    "properties": {
-                        "id": {"type": "keyword"},
-                        "name": {"type": "keyword"},
-                        "username": {"type": "keyword"},
-                        "bio": {"type": "text"},
-                        "location": {"type": "keyword"},
-                        "url": {"type": "text"},
-                        "join_datetime": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
-                        "tweets": {"type": "integer"},
-                        "following": {"type": "integer"},
-                        "followers": {"type": "integer"},
-                        "likes": {"type": "integer"},
-                        "media": {"type": "integer"},
-                        "private": {"type": "integer"},
-                        "verified": {"type": "integer"},
-                        "avatar": {"type": "text"},
-                        "background_image": {"type": "text"},
-                        "session": {"type": "keyword"},
-                        "geo_user": {"type": "geo_point"}
-                        }
-                    },
-                    "settings": {
-                        "number_of_shards": 1
-                    }
+            "mappings": {
+                "properties": {
+                    "id": {"type": "keyword"},
+                    "name": {"type": "keyword"},
+                    "username": {"type": "keyword"},
+                    "bio": {"type": "text"},
+                    "location": {"type": "keyword"},
+                    "url": {"type": "text"},
+                    "join_datetime": {"type": "date", "format": "yyyy-MM-dd HH:mm:ss"},
+                    "tweets": {"type": "integer"},
+                    "following": {"type": "integer"},
+                    "followers": {"type": "integer"},
+                    "likes": {"type": "integer"},
+                    "media": {"type": "integer"},
+                    "private": {"type": "integer"},
+                    "verified": {"type": "integer"},
+                    "avatar": {"type": "text"},
+                    "background_image": {"type": "text"},
+                    "session": {"type": "keyword"}
                 }
-        with nostdout():
-            resp = instance.indices.create(index=config.Index_users, body=user_body, ignore=400)
+            },
+            "settings": {
+                "number_of_shards": 2
+            }
+        }
+        resp = instance.indices.create(index=config.Index_users, body=user_body, ignore=400)
         return handleIndexResponse(resp)
     else:
         print("[x] error index pre-creation :: storage.elasticsearch.createIndex")
         return False
-
-@contextlib.contextmanager
-def nostdout():
-    savestdout = sys.stdout
-    sys.stdout = RecycleObject()
-    yield
-    sys.stdout = savestdout
 
 def weekday(day):
     weekdays = {
@@ -193,9 +172,36 @@ def weekday(day):
 
     return weekdays[day]
 
+def IndexWithRetries(actions, config):
+    global _es
+    
+    retries = 0
+    naptime = 10
+    while retries < 10:
+        try:
+            if _es is False:
+                _es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
+            
+            helpers.bulk(_es, actions, chunk_size=2000, request_timeout=60)
+            break
+
+        except helpers.errors.BulkIndexError:
+            print("\n" + traceback.format_exc())
+            #print("", end="F", flush=True)
+            time.sleep(naptime)
+            retries = retries + 5
+        except:
+            retries = retries + 1
+            _es = False
+            print("\n" + traceback.format_exc())
+            #print("", end="X", flush=True)
+            time.sleep(naptime)
+            naptime = round(naptime * 1.5)
+
 def Tweet(Tweet, config):
     global _index_tweet_status
     global _is_near_def
+    global _es
     date_obj = datetime.strptime(Tweet.datetime, "%Y-%m-%d %H:%M:%S %Z")
 
     actions = []
@@ -216,7 +222,6 @@ def Tweet(Tweet, config):
                 "created_at": Tweet.datetime,
                 "date": dt,
                 "timezone": Tweet.timezone,
-                "place": Tweet.place,
                 "tweet": Tweet.tweet,
                 "language": Tweet.lang,
                 "hashtags": Tweet.hashtags,
@@ -253,30 +258,9 @@ def Tweet(Tweet, config):
     if Tweet.thumbnail:
         j_data["_source"].update({"thumbnail": Tweet.thumbnail})
     if Tweet.mentions:
-        _mentions = []
-        for mention in Tweet.mentions:
-            _mentions.append(mention)
-        j_data["_source"].update({"mentions": _mentions})
+        j_data["_source"].update({"mentions": Tweet.mentions})
     if Tweet.urls:
-        _urls = []
-        for url in Tweet.urls:
-            _urls.append(url)
-        j_data["_source"].update({"urls": _urls})
-    if config.Near or config.Geo:
-        if not _is_near_def:
-            __geo = ""
-            __near = ""
-            if config.Geo:
-                __geo = config.Geo
-            if config.Near:
-                __near = config.Near
-            _is_near_def = getLocation(__near + __geo, near=True)
-        if _near:
-            j_data["_source"].update({"geo_near": _near})
-    if Tweet.place:
-        _t_place = getLocation(Tweet.place)
-        if _t_place:
-            j_data["_source"].update({"geo_tweet": getLocation(Tweet.place)})
+        j_data["_source"].update({"urls": Tweet.urls})
     if Tweet.source:
         j_data["_source"].update({"source": Tweet.Source})
     if config.Translate:
@@ -286,11 +270,11 @@ def Tweet(Tweet, config):
 
     actions.append(j_data)
 
-    es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
+    if _es is False:
+        _es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
     if not _index_tweet_status:
-        _index_tweet_status = createIndex(config, es, scope="tweet")
-    with nostdout():
-        helpers.bulk(es, actions, chunk_size=2000, request_timeout=200)
+        _index_tweet_status = createIndex(config, _es, scope="tweet")
+    IndexWithRetries(actions, config)
     actions = []
 
 def Follow(user, config):
@@ -314,11 +298,11 @@ def Follow(user, config):
             }
     actions.append(j_data)
 
-    es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
+    if _es is False:
+        _es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
     if not _index_follow_status:
-        _index_follow_status = createIndex(config, es, scope="follow")
-    with nostdout():
-        helpers.bulk(es, actions, chunk_size=2000, request_timeout=200)
+        _index_follow_status = createIndex(config, _es, scope="follow")
+    IndexWithRetries(actions, config)
     actions = []
 
 def UserProfile(user, config):
@@ -349,16 +333,11 @@ def UserProfile(user, config):
                 "session": config.Essid
                 }
             }
-    if config.Location:
-        if not _is_location_def:
-            _is_location_def = getLocation(user.location, location=True)
-        if _location:
-            j_data["_source"].update({"geo_user": _location})
     actions.append(j_data)
 
-    es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
+    if _es is False:
+        _es = Elasticsearch(config.Elasticsearch, verify_certs=config.Skip_certs)
     if not _index_user_status:
-        _index_user_status = createIndex(config, es, scope="user")
-    with nostdout():
-        helpers.bulk(es, actions, chunk_size=2000, request_timeout=200)
+        _index_user_status = createIndex(config, _es, scope="user")
+    IndexWithRetries(actions, config)
     actions = []
