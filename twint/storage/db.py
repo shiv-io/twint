05a31c9b01bfa0f2f41009dd62a0387ddc5e01e8
Chris Partridge <chris@partridge.tech>
2022-02-07 03:53:35 -0500

    Update db.py
    Extended version of https://github.com/twintproject/twint/issues/1343

--- twint/storage/db.py
+++ twint/storage/db.py
@@ -261,7 +261,7 @@ def tweets(conn, Tweet, config):
                     Tweet.username,
                     Tweet.name,
                     Tweet.link,
-                    ",".join(Tweet.mentions),
+                    ",".join([json.dumps(mention) for mention in Tweet.mentions]),
                     ",".join(Tweet.hashtags),
                     ",".join(Tweet.cashtags),
                     ",".join(Tweet.urls),
